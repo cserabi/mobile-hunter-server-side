4ced67c98be7015d27bfcd251cc54c6bb60f03f1
cserabi <rabiulislamhstucse@gmail.com>
2021-11-16 00:34:56 +0600

    new

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_password}@cluster0.chwoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.chwoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run() {
   try {
