309b172e7b48f48833fe62ca25aabbe38bea6efe
cserabi <rabiulislamhstucse@gmail.com>
2023-05-20 11:14:49 +0600

    'update-product-manage'

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+
+const bodyParser = require("body-parser");
+
 const app = express();
 require("dotenv").config();
-
 const cors = require("cors");
+const SSLCommerzPayment = require('sslcommerz-lts')
+
 const ObjectId = require("mongodb").ObjectId;
 
 const { MongoClient } = require("mongodb");
@@ -129,18 +133,20 @@ async function run() {
     app.put("/manageProducts/:id", (req, res) => {
       userCollection
         .updateOne(
-          {_id:ObjectId(req.params.id)},
-          { $set: {
-             Name: req.body.Name,
-             des: req.body.des ,
-             price:req.body.price,
-             warranty:req.body.warranty,
-             piclink:req.body.piclink
-            
-            } }
+          { _id: ObjectId(req.params.id) },
+          {
+            $set: {
+              Name: req.body.Name,
+              des: req.body.des,
+              price: req.body.price,
+              warranty: req.body.warranty,
+              piclink: req.body.piclink
+
+            }
+          }
         )
         .then(result => {
-          res.send(result.modifiedCount>0);
+          res.send(result.modifiedCount > 0);
         });
     });
 
@@ -217,6 +223,112 @@ async function run() {
       console.log("load user with id: ", id);
       res.send(productName);
     });
+
+    //Payment Getway 
+
+    // app.use(bodyParser.urlencoded({
+    //   extened: false
+    // }));
+
+    // app.use(bodyParser.json());
+
+
+    app.use("/ssl-request", async (req, res, next) => {
+
+      const data = {
+        total_amount: 100,
+        currency: 'EUR',
+        tran_id: 'REF123',
+        success_url: `${process.env.PORT}/ssl-payment-success`,
+        fail_url: `${process.env.PORT}/ssl-payment-failure`,
+        cancel_url: `${process.env.PORT}/ssl-payment-cancel`,
+        ipn_url: `${process.env.PORT}/ssl-payment-ipn`,
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: 'cust@yahoo.com',
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+        multi_card_name: 'mastercard',
+        value_a: 'ref001_A',
+        value_b: 'ref002_B',
+        value_c: 'ref003_C',
+        value_d: 'ref004_D'
+      };
+      const sslcz = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWORD, false) //true for live default false for sandbox
+      sslcz.init(data).then(apiResponse => {
+        //process the response that got from sslcommerz 
+        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+
+        let GatewayPageURL = apiResponse.GatewayPageURL
+
+
+
+        if (GatewayPageURL) {
+          return res.status(200).redirect(GatewayPageURL);
+        }
+
+        else {
+          return res.status(400).json(
+            {
+              message: "SSL session was not successful"
+            }
+          )
+
+        }
+      });
+
+    });
+
+    app.post("/ssl-payment-success", async (req, res, next) => {
+      return res.status(200).json
+        ({
+          data: req.body
+        })
+    });
+
+
+    app.post("/ssl-payment-failure", async (req, res, next) => {
+      return res.status(400).json
+        ({
+          data: req.body
+        })
+    });
+
+    app.post("/ssl-payment-cancel", async (req, res, next) => {
+      return res.status(200).json
+        ({
+          data: req.body
+        })
+    });
+
+    app.post("/ssl-payment-ipn", async (req, res, next) => {
+      return res.status(200).json
+        ({
+          data: req.body
+        })
+    });
+
+
+
+
+
+
   } finally {
     //await client.close();
   }
